1f71b086a101ba933ed0e98ce8832b8c34e23bd1
sirajkhan8868 <sirajkhan8868447@gmail.com>
2024-10-07 01:13:11 -0700

    admin page

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\Auth\DashboardController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/logout', function () {
+    auth()->logout();
+    // return view('welcome');
+});
+
+Auth::routes([
+    'register'=>false
+]);
+
+Route::get('dashboard',[DashboardController::class,'dashboard'])->name('dashboard');
